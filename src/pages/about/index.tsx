fab51c58e76db3f665a6fec09f8cb4fea8de7763
Mikael Aditya N <mikaeladityan.99@gmail.com>
2024-08-26 09:59:02 +0700

    Update About Page View

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,3 +1,4 @@
+import { Meteors } from "@/components/ui/meteors";
 import {
 	IconBrandGithub,
 	IconBrandInstagram,
@@ -87,7 +88,7 @@ const AboutPage = () => {
 				</div>
 			</section>
 
-			<main className="relative w-8/12 mx-auto h-screen border border-white/20 rounded-3xl mt-20 px-14 pt-20 top-10">
+			<main className="relative w-8/12 mx-auto border border-white/20 rounded-3xl mt-20 px-14 pt-20 top-10">
 				<div className="absolute -top-4 left-10">
 					<div className="px-5 py-2 rounded-full bg-stone-700 text-stone-400 text-[11px] font-bold">ABOUT</div>
 				</div>
@@ -101,6 +102,110 @@ const AboutPage = () => {
 							eligendi fuga necessitatibus sit veniam magnam, iste, ut dignissimos sequi incidunt at. Ullam placeat
 							suscipit earum.
 						</p>
+
+						<h2 className={`${H2.className} text-2xl mt-10`}>Certification 🌟</h2>
+						<div className="mt-10 grid grid-cols-2 gap-10">
+							<div className=" w-full relative max-w-full">
+								<div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
+								<div className="relative shadow-xl bg-black/70 border border-gray-500/50  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+									<div className="h-5 w-5 rounded-full border flex items-center justify-center mb-4 border-gray-500">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth="1.5"
+											stroke="currentColor"
+											className="h-2 w-2 text-gray-300"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25"
+											/>
+										</svg>
+									</div>
+
+									<h1 className="font-bold text-xl text-white mb-4 relative z-50">ECS Technical Essentials</h1>
+
+									<Image
+										src={"/certif/alibaba_ECS.png"}
+										alt="Certification of Mikael"
+										width={200}
+										height={200}
+										className="w-full h-full relative !z-[1]"
+									/>
+									<Meteors number={20} />
+								</div>
+							</div>
+
+							<div className=" w-full relative max-w-full">
+								<div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
+								<div className="relative shadow-xl bg-black/70 border border-gray-500/50  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+									<div className="h-5 w-5 rounded-full border flex items-center justify-center mb-4 border-gray-500">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth="1.5"
+											stroke="currentColor"
+											className="h-2 w-2 text-gray-300"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25"
+											/>
+										</svg>
+									</div>
+
+									<h1 className="font-bold text-xl text-white mb-4 relative z-50">Construct a Dynamic Website</h1>
+
+									<Image
+										src={"/certif/cloud_computing.png"}
+										alt="Certification of Mikael"
+										width={200}
+										height={200}
+										className="w-full h-full relative !z-[1]"
+									/>
+									<Meteors number={20} />
+								</div>
+							</div>
+
+							<div className=" w-full relative max-w-full">
+								<div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
+								<div className="relative shadow-xl bg-black/70 border border-gray-500/50  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+									<div className="h-5 w-5 rounded-full border flex items-center justify-center mb-4 border-gray-500">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth="1.5"
+											stroke="currentColor"
+											className="h-2 w-2 text-gray-300"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25"
+											/>
+										</svg>
+									</div>
+
+									<h1 className="font-bold text-xl text-white mb-4 relative z-50">Recloud Indonesia 2021</h1>
+
+									<Image
+										src={"/certif/recloud.png"}
+										alt="Certification of Mikael"
+										width={200}
+										height={200}
+										className="w-full h-full relative !z-[1]"
+									/>
+									<Meteors number={20} />
+								</div>
+							</div>
+						</div>
+						<h2 className={`${H2.className} text-2xl mt-10`}>My Interest 🌟</h2>
+						<h2 className={`${H2.className} text-2xl mt-10`}>Academic Qualification 🌟</h2>
 					</article>
 				</div>
 			</main>
