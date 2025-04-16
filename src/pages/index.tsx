b3f67ae425108f656d9a0e1e425a517e3f45eee7
Mikael Aditya N <mikaeladityan.99@gmail.com>
2025-04-17 01:30:05 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 		<>
 			<Header />
 
-			<section className="h-full w-full  bg-black  bg-grid-white/[0.1] relative flex items-center justify-center">
+			<section className="h-full w-full hidden md:flex bg-black  bg-grid-white/[0.1] relative items-center justify-center">
 				<div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 				<AboutSection />
 			</section>
